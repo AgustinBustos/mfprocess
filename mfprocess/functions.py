372d1916d1b202238cf645ff5cb1a53766580492
Agustin Bustos Barton <88463929+AgustinBustos@users.noreply.github.com>
2024-05-07 14:58:34 -0300

    nueva funcion para los paths

--- mfprocess/functions.py
+++ mfprocess/functions.py
@@ -1,6 +1,7 @@
-def hereyourfunction():
-    print('hi!!!')
-
+def input_path(message=""):  #translate copy path of windows
+  raw_s=input(message)
+  raw_s=raw_s.replace(r'"','')
+  return "/content/drive/Shareddrives/"+'/'.join(raw_s.split("\\")[2:])
 
 
 
