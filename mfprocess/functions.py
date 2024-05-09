816bff3024c6b0a6dcbaa7b2bc256470f10a0896
Agustin Bustos Barton <88463929+AgustinBustos@users.noreply.github.com>
2024-05-09 10:21:44 -0300

    Update functions.py

--- mfprocess/functions.py
+++ mfprocess/functions.py
@@ -1,5 +1,6 @@
 def input_path(message=""):  #translate copy path of windows
-  raw_s=input(message)
+  
+  raw_s=input(message) if message=="" else message
   raw_s=raw_s.replace(r'"','')
   return "/content/drive/Shareddrives/"+'/'.join(raw_s.split("\\")[2:])
 
