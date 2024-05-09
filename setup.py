e769452ecda0a7ff4ec605d016bc9a55a5ae0c2b
Agustin Bustos Barton <88463929+AgustinBustos@users.noreply.github.com>
2024-05-09 10:25:44 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['numpy','pandas'],
+    install_requires=['numpy','pandas','pycountry','time'],
     keywords=['pypi', 'cicd', 'python'],
     classifiers=[
         "Development Status :: 1 - Planning",
